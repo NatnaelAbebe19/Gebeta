b059f81cfc02360e6cb00865af58fca393ccc459
jud <joejud@gmail.com>
2024-06-10 02:19:51 +0300

    profile picture and a bell is added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BellIcon } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text className="text-amber-500">HomeScreen all the time </Text>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/profile picture.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+          <BellIcon size={hp(4)} color="gray" />
+        </View>
+      </ScrollView>
     </View>
   );
 }
