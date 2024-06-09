ed91f644d2871d06f9e9524b5d58a44e91a63696
jud <joejud@gmail.com>
2024-06-10 02:18:44 +0300

    welcome page is animated

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,15 +1,33 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-// import Animated, { useSharedValue } from "react-native-reanimated";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 export default function WelcomeScreen() {
-  //   const ring1Padding = useSharedValue(0);
-  //   const ring2Padding = useSharedValue(0);
+  const ring1Padding = useSharedValue(0);
+  const ring2Padding = useSharedValue(0);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    ring1Padding.value = 0;
+    ring2Padding.value = 0;
+    setTimeout(
+      () => (ring1Padding.value = withSpring(ring1Padding.value + hp(5))),
+      100
+    );
+    setTimeout(
+      () => (ring2Padding.value = withSpring(ring2Padding.value + hp(5.5))),
+      300
+    );
+    setTimeout(() => navigation.navigate("Home"), 2500);
+  });
+
   return (
     <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
       <StatusBar style="light" />
@@ -24,14 +42,20 @@ export default function WelcomeScreen() {
           Gebeta
         </Text>
       </View>
-      <View className="bg-white/20 rounded-full" style={{ padding: hp(5.5) }}>
-        <View className="bg-white/20 rounded-full" style={{ padding: hp(5) }}>
+      <Animated.View
+        className="bg-white/20 rounded-full"
+        style={{ padding: ring2Padding }}
+      >
+        <Animated.View
+          className="bg-white/20 rounded-full"
+          style={{ padding: ring1Padding }}
+        >
           <Image
             source={require("../../assets/intro.png")}
             style={{ width: hp(20), height: hp(20) }}
           />
-        </View>
-      </View>
+        </Animated.View>
+      </Animated.View>
 
       {/* Title and punchline */}
       <View className="flex items-center space-y-2">
