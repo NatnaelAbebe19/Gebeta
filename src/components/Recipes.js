171a1b41fa861d35a08856907019c23847c94c98
jud <joejud@gmail.com>
2024-06-14 04:19:22 +0300

    the recipe is started

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Recipes() {
   return (
-    <View>
-      <Text>Recipes</Text>
+    <View className="mx-4 space-y-3">
+      <Text
+        style={{ fontSize: hp(3) }}
+        className="font-semibold text-neutral-600 "
+      >
+        Recipes
+      </Text>
     </View>
   );
 }
