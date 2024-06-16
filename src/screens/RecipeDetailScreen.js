586562aa376ee47448d82f645bf6537e3ade413c
jud <joejud@gmail.com>
2024-06-16 11:32:25 +0300

    update

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { CachedImage } from "../helpers/image";
 import {
@@ -9,11 +9,35 @@ import {
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import Loading from "../components/Loading";
 
 export default function RecipeDetailScreen(props) {
   let item = props.route.params;
   const [isFavourite, setIsFavourite] = useState(false);
   const navigation = useNavigation();
+  const [meal, setMeal] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getMealData(item.idMeal);
+  }, []);
+
+  const getMealData = async (id) => {
+    try {
+      const response = await axios.get(
+        `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      //   console.log(response.data);
+      if (response && response.data) {
+        setMeal(response.data.meals[0]);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log("err: ", err.message);
+    }
+  };
+
   return (
     <ScrollView
       className="bg-white flex-1"
@@ -53,6 +77,14 @@ export default function RecipeDetailScreen(props) {
           />
         </TouchableOpacity>
       </View>
+
+      {loading ? (
+        <Loading className="mt-16" />
+      ) : (
+        <View>
+          <Text>Display Meal data</Text>
+        </View>
+      )}
     </ScrollView>
   );
 }
