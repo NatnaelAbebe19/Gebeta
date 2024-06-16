24e16cb176b35659d2af968ebf213153fdd9f660
jud <joejud@gmail.com>
2024-06-16 11:14:46 +0300

    to the cache the images that are fetched from the api

---
+++ src/helpers/image.js
@@ -0,0 +1,37 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+
+import Animated from "react-native-reanimated";
+
+export const CachedImage = (props) => {
+  const [cachedSource, setChachedSource] = useState(null);
+  const { uri } = props;
+
+  useEffect(() => {
+    const getCachedImage = async () => {
+      try {
+        const cachedImageData = await AsyncStorage.getItem(uri);
+        if (cachedImageData) {
+          setChachedSource({ uri: cachedImageData });
+        } else {
+          const response = await fetch(uri);
+          const imageBlob = await response.blob();
+          const base64Data = await new Promise((resolve) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(imageBlob);
+            reader.onloadend = () => {
+              resolve(reader.result);
+            };
+          });
+          await AsyncStorage.setItem(uri, base64Data);
+          setChachedSource({ uri: base64Data });
+        }
+      } catch (error) {
+        console.log("Errpr caching image: ", error);
+        setChachedSource({ uri });
+      }
+    };
+    getCachedImage();
+  }, []);
+  return <Animated.Image source={cachedSource} {...props} />;
+};
